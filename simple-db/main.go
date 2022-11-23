76c24cb4e3afdc2780e8334ae03d6dd2659a4951
MartinVNavarro <martinvnavarro@gmail.com>
2022-11-23 09:24:05 -0800

    simple comments

--- simple-db/main.go
+++ simple-db/main.go
@@ -6,11 +6,13 @@ import (
 )
 
 type Account struct {
+	// gorm field tag to mark ID as a PK upon insert
 	ID   int `gorm:"primaryKey"`
 	Name string
 }
 
 func main() {
+	// Replace any of the paramaters accordingly
 	dsn := "host=localhost user=admin password=admin dbname=lazy port=5432 sslmode=disable timezone=America/Los_Angeles"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
@@ -18,12 +20,15 @@ func main() {
 		panic("failed to connect to db")
 	}
 
+	// Migrate schema to db
 	err = db.AutoMigrate(&Account{})
 
+	// Create a new account Record
 	db.Create(&Account{ID: 1, Name: "name"})
 
 	var account Account
 
+	// Finds first occurance of an Account with a primary key 1
 	db.First(&account, 1)
 
 	println("Found an account : " + account.Name)
